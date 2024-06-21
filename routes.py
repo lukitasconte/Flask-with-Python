aafc41f2b8c091ffef45213412966b0ab000019f
lukitasconte <138738151+lukitasconte@users.noreply.github.com>
2024-06-21 01:58:51 -0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,18 +1,11 @@
+# routes.py
+
 from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
-# Definir las rutas dentro del blueprint
 @bp.route('/')
 def index():
     return render_template('index.html')
-
-@bp.route('/about')
-def about():
-    return "Esta es la página de acerca de nosotros."
-
-@bp.route('/user/<username>')
-def user_profile(username):
-    return f"Perfil de usuario: {username}"
 
 
