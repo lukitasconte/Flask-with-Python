874f763d8ee65c40d7c268b740ff5fd0c21d3edf
lukitasconte <138738151+lukitasconte@users.noreply.github.com>
2024-06-24 15:08:47 -0300

    models.py

--- models.py
+++ models.py
@@ -2,12 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 class Task(db.Model):
-    __tablename__ = 'task'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        return f'<Task {self.description}>'
-
